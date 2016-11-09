5939c53c120796992287490e91fb774600caa5e2
Brunno Vodola <bvodola@gmail.com>
2016-11-09 08:33:06 -0200

    Trying to fix duplicate rendering

--- lib/client.jsx
+++ lib/client.jsx
@@ -20,23 +20,23 @@ const ReactRouterSSR = {
       const attributes = clientOptions.rootElementAttributes instanceof Array ? clientOptions.rootElementAttributes : [];
       let rootElement = document.getElementById(rootElementName);
 
-      // In case the root element doesn't exist, let's create it
-      if (!rootElement) {
-        rootElement = document.createElement(rootElementType);
-        rootElement.id = rootElementName;
-
-        // check if a 2-dimensional array was passed... if not, be nice and handle it anyway
-        if(attributes[0] instanceof Array) {
-          // set attributes
-          for(var i = 0; i < attributes.length; i++) {
-            rootElement.setAttribute(attributes[i][0], attributes[i][1]);
-          }
-        } else if (attributes.length > 0){
-          rootElement.setAttribute(attributes[0], attributes[1]);
-        }
-
-        document.body.appendChild(rootElement);
-      }
+      // // In case the root element doesn't exist, let's create it
+      // if (!rootElement) {
+      //   rootElement = document.createElement(rootElementType);
+      //   rootElement.id = rootElementName;
+      //
+      //   // check if a 2-dimensional array was passed... if not, be nice and handle it anyway
+      //   if(attributes[0] instanceof Array) {
+      //     // set attributes
+      //     for(var i = 0; i < attributes.length; i++) {
+      //       rootElement.setAttribute(attributes[i][0], attributes[i][1]);
+      //     }
+      //   } else if (attributes.length > 0){
+      //     rootElement.setAttribute(attributes[0], attributes[1]);
+      //   }
+      //
+      //   document.body.appendChild(rootElement);
+      // }
 
       // Rehydrate data client side, if desired.
       if(typeof clientOptions.rehydrateHook === 'function') {
